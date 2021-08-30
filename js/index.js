7290f40833e7df159d896210f8bc2912fcd1acbd
Yerovy Espitia <luyeesgar@gmail.com>
2021-08-29 20:50:47 -0500

    new fonts added & skills text animation added

--- js/index.js
+++ js/index.js
@@ -1,5 +1,10 @@
 const skills = document.getElementById("skills");
 const projects = document.getElementById("projects")
+const textPath = document.getElementById("text-path")
+const textContainer = document.getElementById("text-container")
+const path = document.querySelector(textPath.getAttribute("href"))
+
+const pathLength = path.getTotalLength()
 
 window.addEventListener("scroll", () => {
   const scrolled = window.scrollY;
@@ -13,3 +18,19 @@ window.addEventListener("scroll", () => {
   }
   console.log(scrolled);
 });
+
+const updateTextPathOffset = (offset) => {
+  textPath.setAttribute("startOffset", offset)
+}
+
+updateTextPathOffset(pathLength)
+
+const onScroll = () => {
+  requestAnimationFrame(() => {
+    const rect = textContainer.getBoundingClientRect()
+    const scrollPercent = rect.y / window.innerHeight
+    updateTextPathOffset(scrollPercent * 2 *  pathLength)
+  })
+}
+
+window.addEventListener("scroll", onScroll)
